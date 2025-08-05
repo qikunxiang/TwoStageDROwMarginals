a4d2fe755a2cd18957d8daf45202835be3579e6c
qikun <qikun001@e.ntu.edu.sg>
2025-08-05 13:49:23 +0800

    05/08/2025

--- func/DRO/problems/DRO_task_scheduling.m
+++ func/DRO/problems/DRO_task_scheduling.m
@@ -18,9 +18,9 @@ DRO.stage2 = struct;
 DRO.stage2.V = -speye(N);
 DRO.stage2.W = speye(N);
 DRO.stage2.b = zeros(N, 1);
-DRO.stage2.Lin = sparse(repmat((1:N - 1)', 2, 1), [1:N - 1, 2:N]', ...
-    [ones(N - 1, 1); -ones(N - 1, 1)], N - 1, N);
-DRO.stage2.qin = ones(N - 1, 1);
+DRO.stage2.Lin = sparse([(1:N)'; (2:N)'], [(1:N)'; (1:N - 1)'], ...
+    [-ones(N, 1); ones(N - 1, 1)], N, N);
+DRO.stage2.qin = ones(N, 1);
 DRO.stage2.lb = zeros(N, 1);
 DRO.stage2.ub = (N:(-1):1)';
 
